f0f6976c51773ae95272145422585356ea874c07
ilandbz <ilandbz@gmail.com>
2023-11-22 10:45:58 -0500

    aa

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends Controller
     public function __invoke(){
         $data['fechaActual'] = Carbon::now(); 
         $fechaActual = Carbon::now();
-        return $fechaActual->formatLocalized('%A %d %B %Y');
+        return $fechaActual->diffForHumans();
         // $data['sliders']=Slider::where('es_activo', 1)->get();
         // $data['secciones']=SeccionesPrincipal::where('es_activo', 1)->get();
         // $data['organizacion'] = Organizacion::first();
