6b5a3edc35bccd8fe623743dad0c2eeccce95256
ilandbz <ilandbz@gmail.com>
2023-12-07 10:03:41 -0500

    correccion

--- resources/views/paginas/municipalidadalcalde.blade.php
+++ resources/views/paginas/municipalidadalcalde.blade.php
@@ -9,16 +9,16 @@
                     <div class="single-teacher mb-20 text-center">
                         <div class="teacher-social">
                             <ul class="social">
-                                <li><a href="{{$alcalde->link}}"><i class="fas fa-link"></i></a></li>
+                                <li><a href="{{$alcalde->link}}" target="_blank"><i class="fas fa-link"></i></a></li>
                             </ul>
                         </div>
                         <div class="teacher-image">
-                            <a href="teacher-details.html">
+                            <a href="{{$alcalde->link}}" target="_blank">
                                 <img src="{{asset('storage/fotos/'.$alcalde->foto);}}" width="266" height="359" alt="teacher">
                             </a>
                         </div>
                         <div class="teacher-content">
-                            <h4 class="name"><a href="teacher-details.html">{{$alcalde->titulo}}</a></h4>
+                            <h4 class="name"><a href="{{$alcalde->link}}">{{$alcalde->titulo}}</a></h4>
                             <span class="designation">{{$alcalde->cargo->nombre}}</span>
                             <ul >
                                 <li class="list-group-item"><strong>DNI:</strong> {{$alcalde->dni}}</li>
