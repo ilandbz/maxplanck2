df08ef2989f7fc2389fadfa25b15b77c574b3e72
ilandbz <ilandbz@gmail.com>
2024-01-18 09:18:30 -0500

    observacion cynthia gerencia municipal eliminar items izquierdo

--- resources/js/Components/NavBar.vue
+++ resources/js/Components/NavBar.vue
@@ -56,13 +56,13 @@ import { useAutenticacion } from '@/Composables/autenticacion';
                 <li class="nav-item dropdown user-menu">
                     <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
                         <img src="/imagenes/ironman.png" class="user-image rounded-circle shadow" alt="User Image">
-                        <span class="d-none d-md-inline">{{ usuario.username }}</span>
+                        <span class="d-none">{{ usuario.name }}</span>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
                         <li class="user-header text-bg-primary">
                             <img src="/imagenes/ironman.png" class="rounded-circle shadow" alt="User Image">
                             <p>
-                                {{ usuario.username }}
+                                {{ usuario.name }}
                             </p>
                         </li>
                         <li class="user-body">
