35fa6caae9566d1eabb1e4c4ff2a899d3ebe5b8b
ilandbz <ilandbz@gmail.com>
2024-01-28 22:56:19 -0500

    aa

--- database/seeders/EntradaSeeder.php
+++ database/seeders/EntradaSeeder.php
@@ -32,9 +32,64 @@ class EntradaSeeder extends Seeder
             'contenido'   => '<section class="notice-area mb-4">
             <div class="container">
             <div class="row">
-                <div class="col-lg-12">
+                <div class="col-lg-4">
                 <div class="section-title-2">
-                    <h2 class="title">VALORES</h2><p>COMPROMISO</p><p>TRANSPARENCIA</p><p>DEMOCRACIA PARTICIPATIVA</p><p>HONRADEZ</p><p>RESPETO</p><p>EMPATIA</p><p><br></p></div></div></div><div class="notice-content">
+                    <h2 class="title">VALORES</h2><p>COMPROMISO</p><p>TRANSPARENCIA</p>
+                    <p>DEMOCRACIA PARTICIPATIVA</p><p>HONRADEZ</p><p>RESPETO</p><p>EMPATIA</p><p><br></p></div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="row">
+                        <div class="col-lg-4 col-sm-6">
+                            <div class="single-gallery mt-30">
+                                <a class="image-popup" href="edumate/assets/images/gallery/1.jpg">
+                                    <img src="edumate/assets/images/gallery/1.jpg" width="371" height="257" alt="gallery">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-sm-6">
+                            <div class="single-gallery mt-30">
+                                <a class="image-popup" href="edumate/assets/images/gallery/2.jpg">
+                                    <img src="edumate/assets/images/gallery/2.jpg" width="371" height="257" alt="gallery">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-sm-6">
+                            <div class="single-gallery mt-30">
+                                <a class="image-popup" href="edumate/assets/images/gallery/3.jpg">
+                                    <img src="edumate/assets/images/gallery/3.jpg" width="371" height="257" alt="gallery">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-sm-6">
+                            <div class="single-gallery mt-30">
+                                <a class="image-popup" href="edumate/assets/images/gallery/4.jpg">
+                                    <img src="edumate/assets/images/gallery/4.jpg" width="371" height="257" alt="gallery">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-sm-6">
+                            <div class="single-gallery mt-30">
+                                <a class="image-popup" href="edumate/assets/images/gallery/5.jpg">
+                                    <img src="edumate/assets/images/gallery/5.jpg" width="371" height="257" alt="gallery">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-sm-6">
+                            <div class="single-gallery mt-30">
+                                <a class="image-popup" href="edumate/assets/images/gallery/6.jpg">
+                                    <img src="edumate/assets/images/gallery/6.jpg" width="371" height="257" alt="gallery">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-sm-6">
+                            <div class="single-gallery mt-30">
+                                <a class="image-popup" href="edumate/assets/images/gallery/7.jpg">
+                                    <img src="edumate/assets/images/gallery/7.jpg" width="371" height="257" alt="gallery">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             </div>
         </section>'
